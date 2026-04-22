e9dbbe17213bb7dcbbb14788968879713c11d3f0
shivraj-chavan <shivrajchavan1902@gmail.com>
2026-04-22 15:07:33 +0000

    updated chatbot

--- src/constance.js
+++ src/constance.js
@@ -1,8 +1,8 @@
 const CONFIG={
-    // "API_BASE_URL":"http://localhost:5009/api",
+    "API_BASE_URL":"https://api.pod-sphere.com/api",
     // "API_BASE_URL":"http://31.97.226.22:5009/api",
     // "API_IMAGE_BASE_URL":"http://localhost:5009",
-    "API_BASE_URL":"http://69.62.84.113:5033/api",
+    // "API_BASE_URL":"http://69.62.84.113:5033/api",
     // "API_IMAGE_BASE_URL":"http://69.62.84.113:5006",
 }
 
